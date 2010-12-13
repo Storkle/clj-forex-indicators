d756448864b0f8f9044adcb52e64f7176c36f036
Storkle <user@email>
2010-12-13 17:44:43 -0600

    fix SMA to not test now

--- test/indicators/test/SMA.clj
+++ test/indicators/test/SMA.clj
@@ -1,16 +1,7 @@
 (ns indicators.test.SMA
-  (:use [indicators.collection.stream]
+  (:use 
 	[clojure.test])
   (:import (indicators SMA VMA)
 	   (indicators.collection PriceStream)
-	   indicators._test.Csv))
- 
-(let [stream (repeat 10000 1.0)]
-  (def pstream (new-pstream stream stream stream stream)))
-
-(def stream (Csv/get "/home/seth/Desktop/aud_nzd.csv"))
- 
-(def sequence (.Open stream)) 
-(def sma (SMA. stream sequence 20))
-
+	   indicators.utils.Csv))
  
