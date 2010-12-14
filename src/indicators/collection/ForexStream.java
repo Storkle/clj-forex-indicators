daaa1d7ce606189dd2c634ce93a96f9762eb321c
Storkle <user@email>
2010-12-13 22:40:21 -0600

    added ForexStream

---
+++ src/indicators/collection/ForexStream.java
@@ -0,0 +1,7 @@
+package indicators.collection;
+
+public class ForexStream extends PriceStream {
+	public String symbol = "";
+	public int timeframe = 60; 
+	public long headTime = 0;  
+}
--- test/indicators/test/SMA.clj
+++ test/indicators/test/SMA.clj
@@ -1,6 +1,5 @@
 (ns indicators.test.SMA
-  (:use 
-	[clojure.test])
+  (:use [clojure.test])
   (:import (indicators SMA VMA)
 	   (indicators.collection PriceStream)
 	   indicators.utils.Csv))
