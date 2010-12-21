cafb7564f18b4f630a16fcfabc221c8f4ddfd94b
Storkle <user@email>
2010-12-21 07:40:26 -0600

    fix project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject indicators "0.1"
+(defproject clj-forex-indicators "0.1"
   :description "indicator library for clj-forex"
   :dev-dependencies [[native-deps "1.0.4"] [swank-clojure "1.3.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.2.0"]
