2495491f06eddeff5230c9aee53a9628abe093d1
Storkle <user@email>
2010-12-21 07:42:41 -0600

    move to dev-dependency

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject clj-forex-indicators "0.1"
   :description "indicator library for clj-forex"
-  :dev-dependencies [[native-deps "1.0.4"] [swank-clojure "1.3.0-SNAPSHOT"]]
+  :dev-dependencies [[native-deps "1.0.4"]
+		     [scalatest "1.2"]
+		     [swank-clojure "1.3.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [scala "2.8.1"]
-		 [scalatest "1.2"]])
+		 [scala "2.8.1"]])
