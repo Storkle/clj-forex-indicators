e74df1d0b177b413d32affeb009fa2d3d3e4a509
storkle <seth@Laurel.(none)>
2010-12-26 15:35:15 -0600

    update project.clj to work with clojars

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject clj-forex-indicators "0.1"
-  :description "indicator library for clj-forex"
-  :dev-dependencies [[native-deps "1.0.4"]
-		     [scalatest "1.2"]
+(defproject org.clojars.storkle/clj-forex-indicators "0.1"
+  :description "indicator library for clj-forex" 
+  :dev-dependencies [[lein-clojars "0.6.0"]  
+		     [org.scalatest/scalatest "1.2"]
 		     [swank-clojure "1.3.0-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [scala "2.8.1"]])
+		 [org.scala-lang/scala-library "2.8.1"]]
+  :repositories {"scalatest" "http://www.scala-tools.org/repo-releases/"})
+      
